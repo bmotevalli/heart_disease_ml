c3c28f53605f3cfe677c543b057b762739deeeca
Mona22469 <83955167+Mona22469@users.noreply.github.com>
2021-05-10 22:07:05 +0800

    upaditng helppage

--- src/api/predictHeartDisease.js
+++ src/api/predictHeartDisease.js
@@ -3,5 +3,6 @@ import * as Api from "axios";
 const url = "https://n57j0h4ao4.execute-api.ap-southeast-2.amazonaws.com/test/heart-disease";
 
 export const predictHeartDisease = async (req) => {
+  console.log(req)
     return await Api.post(url, req)
   };
--- src/containers/Help/Help.js
+++ src/containers/Help/Help.js
@@ -5,8 +5,8 @@ class Help extends Component {
     render() { 
         return (
             <React.Fragment>
-            <h3>Heart disease is Australia’s leading single cause of death, with 18,590 deaths attributed to heart disease in Australia in 2017. Heart disease kills one Australian every 28 minutes¹.</h3>
-            <img src="https://www.heartresearch.com.au/wp-content/uploads/2016/08/shutterstock_208215142-500x250.jpg"/>
+            <h3>Farhad m,adresedoost.</h3>
+            <img src="https://git-scm.com/images/icons/nav-read-book.png"/>
             <p>Heart disease is an umbrella term for range of conditions that affect your heart. Diseases under the heart disease umbrella include blood vessel diseases, such as coronary artery disease; heart rhythm problems (arrhythmias); and heart defects you’re born with (congenital heart defects), among others.
 The term ‘heart disease’ is often used interchangeably with the term ‘cardiovascular disease’. Cardiovascular disease generally refers to conditions that involve narrowed or blocked blood vessels that can lead to a heart attack, chest pain (angina) or stroke. Other heart conditions, such as those that affect your heart’s muscle, valves or rhythm, also are considered forms of heart disease.
 Keeping your heart healthy, whatever your age, is the most important thing you can do to help prevent and manage heart disease.
